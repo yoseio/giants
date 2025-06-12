31761d95b5e7edf54ea3cc7f457e87af59940d9d
yoseio <98276492+yoseio@users.noreply.github.com>
2025-06-12 14:27:13 +0900

    fix

--- app/papers/add/page.tsx
+++ app/papers/add/page.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { AddPaperForm } from "@/components/AddPaperForm";
+import { AddPaperForm } from "@/components/add-paper-form";
 
 export default function AddPaperPage() {
   return (
